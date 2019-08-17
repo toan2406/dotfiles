afe82f6c6ff3231ef3e67904e8008af69ec7d941
Toan Nguyen <toan2406@gmail.com>
2019-08-18 00:21:49 +0700

    add emacs config

---
+++ emacs/.emacs.d/config/ui.el
@@ -0,0 +1,7 @@
+(straight-use-package 'spacemacs-theme)
+(load-theme 'spacemacs-dark t)
+
+(straight-use-package 'spaceline)
+(require 'spaceline-config)
+(spaceline-emacs-theme)
+(setq powerline-default-separator 'slant)
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,27 @@
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'evil)
+(require 'evil)
+(evil-mode 1)
+
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+
+(global-linum-mode t)
+(load (concat user-emacs-directory "config/ui"))
