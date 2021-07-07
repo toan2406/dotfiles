c6937eb17f2aeb516401d599e2fdd6652cb74fd6
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-07-08 02:51:35 +0700

    update nvim-lua config

--- nvim-lua/.config/nvim/lua/general/mappings.lua
+++ nvim-lua/.config/nvim/lua/general/mappings.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 local MYVIMRC = '$HOME/.config/nvim/init.lua'
 
-api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = true})
+api.nvim_set_keymap('i', 'jj', '<Esc>', {noremap = false})
 api.nvim_set_keymap('n', '<Space>', ':', {noremap = true})
 
 api.nvim_set_keymap('n', '<leader>vc', ':e $MYVIMRC<CR>', {noremap = true, silent = true})
