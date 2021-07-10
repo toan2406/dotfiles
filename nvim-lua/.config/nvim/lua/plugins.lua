8088d7e7e19b2f255e4e5ff378b61b26551d6b1c
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-07-11 00:50:15 +0700

    update nvim-lua config

--- nvim-lua/.config/nvim/lua/plugins.lua
+++ nvim-lua/.config/nvim/lua/plugins.lua
@@ -2,40 +2,41 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Color schemes
   use 'jacoborus/tender.vim'
   use 'andreypopp/vim-colors-plain'
+
+  -- Navigation
   use 'toan2406/vifm.vim'
   use 'justinmk/vim-sneak'
-  use 'b3nj5m1n/kommentary'
-  use 'hrsh7th/nvim-compe'
-  use 'numToStr/Navigator.nvim'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'tpope/vim-surround'
-  use 'windwp/nvim-autopairs'
-  use 'dense-analysis/ale'
-  use 'lukas-reineke/indent-blankline.nvim'
+  use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
+  use 'junegunn/fzf.vim'
 
+  -- Completion
+  use 'hrsh7th/nvim-compe'
+
+  -- Statusline
   use 'itchyny/lightline.vim'
   use 'maximbaz/lightline-ale'
 
+  -- Git support
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
   use 'junegunn/gv.vim'
 
-  use {
-    'junegunn/fzf',
-    run = function() vim.fn['fzf#install']() end
-  }
-  use 'junegunn/fzf.vim'
-
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+  -- Language support
+  use 'dense-analysis/ale'
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
   use 'nvim-treesitter/nvim-treesitter-refactor'
-
   use 'neovim/nvim-lspconfig'
-
   use 'reasonml-editor/vim-reason-plus'
+
+  -- Misc
+  use 'windwp/nvim-autopairs'
+  use 'tpope/vim-surround'
+  use 'norcalli/nvim-colorizer.lua'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'b3nj5m1n/kommentary'
+  use 'numToStr/Navigator.nvim'
 end)
 
