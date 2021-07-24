3c9a9ffbd02933169e99b1bcfcda6e45669484d3
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-07-24 16:34:33 +0700

    update nvim-lua config

--- nvim-lua/.config/nvim/lua/colorschemes/tender.lua
+++ nvim-lua/.config/nvim/lua/colorschemes/tender.lua
@@ -1,5 +1,6 @@
 vim.cmd [[autocmd ColorScheme * highlight LspDiagnosticsDefaultError guifg=#f43753]]
 vim.cmd [[autocmd ColorScheme * highlight LspDiagnosticsDefaultWarning guifg=#ffc24b]]
+vim.cmd [[autocmd ColorScheme * highlight LspDiagnosticsDefaultInformation guifg=#666666]]
 
 vim.cmd [[colorscheme tender]]
 
