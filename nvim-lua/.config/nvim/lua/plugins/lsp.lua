feb7b69bdf659b048cf39f2f03e0a31ab0e04a03
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-08-23 17:28:44 +0700

    update lspconfig

--- nvim-lua/.config/nvim/lua/plugins/lsp.lua
+++ nvim-lua/.config/nvim/lua/plugins/lsp.lua
@@ -16,10 +16,6 @@ end
 
 saga.init_lsp_saga()
 
-api.nvim_set_keymap('n', '<leader>lh', ':Lspsaga hover_doc<CR>', {noremap = true, silent = true})
-api.nvim_set_keymap('n', '<C-f>', ':lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', {noremap = true, silent = true})
-api.nvim_set_keymap('n', '<C-b>', ':lua require("lspsaga.action").smart_scroll_with_saga(-1)<CR>', {noremap = true, silent = true})
-
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -30,8 +26,13 @@ local on_attach = function(client, bufnr)
 
   buf_set_keymap('n', '<leader>lf', '<CMD>lua vim.lsp.buf.formatting()<CR>', opts)
   buf_set_keymap('n', '<leader>ld', '<CMD>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>ll', '<CMD>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
-  if client.name ~= 'efm' then
+  buf_set_keymap('n', '<leader>lh', ':Lspsaga hover_doc<CR>', opts)
+  buf_set_keymap('n', '<C-f>', ':lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', opts)
+  buf_set_keymap('n', '<C-b>', ':lua require("lspsaga.action").smart_scroll_with_saga(-1)<CR>', opts)
+
+  if client.name == 'solargraph' or client.name == 'tsserver' then
     client.resolved_capabilities.document_formatting = false
   end
 end
