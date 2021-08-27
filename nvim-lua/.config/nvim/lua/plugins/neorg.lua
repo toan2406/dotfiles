478bb50a55b946f98ed5d78ebae5c5dd2817f461
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-08-28 01:16:20 +0700

    update neorg config

--- nvim-lua/.config/nvim/lua/plugins/neorg.lua
+++ nvim-lua/.config/nvim/lua/plugins/neorg.lua
@@ -1,6 +1,54 @@
 require('neorg').setup({
   load = {
     ['core.defaults'] = {},
+    ['core.norg.concealer'] = {
+      config = {
+        icons = {
+          todo = {
+            enabled = true,
+            done = {
+              enabled = true,
+              icon = '✓'
+            },
+            pending = {
+              enabled = true,
+              icon = '-'
+            },
+            undone = {
+              enabled = true,
+              icon = ' '
+            },
+          },
+          quote = {
+            enabled = true,
+            icon = '∣'
+          },
+          heading = {
+            enabled = true,
+            level_1 = {
+              enabled = true,
+              icon = '◉'
+            },
+            level_2 = {
+              enabled = true,
+              icon = '○'
+            },
+            level_3 = {
+              enabled = true,
+              icon = '✿'
+            },
+            level_4 = {
+              enabled = true,
+              icon = '•'
+            },
+          },
+          marker = {
+            enabled = true,
+            icon = '!'
+          },
+        }
+      }
+    },
     ['core.keybinds'] = {
       config = {
         default_keybinds = true,
