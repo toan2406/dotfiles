c2f8f7d704913ca70285599051c5bbacf48d5367
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-09-25 16:33:52 +0700

    update nvim-lua config

--- nvim-lua/.config/nvim/lua/plugins/fzf.lua
+++ nvim-lua/.config/nvim/lua/plugins/fzf.lua
@@ -3,7 +3,8 @@ local api = vim.api
 
 g.fzf_history_dir = '~/.local/share/fzf-history'
 
-vim.cmd [[command! -bang -nargs=* Rg call fzf#vim#grep('rg --column --line-number --no-heading --color=always --smart-case --hidden -- '.shellescape(<q-args>), 1, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}), <bang>0)]]
+-- vim.cmd [[command! -bang -nargs=* Rg call fzf#vim#grep('rg --column --line-number --no-heading --color=always --smart-case --hidden -- '.shellescape(<q-args>), 1, fzf#vim#with_preview({'options': '--delimiter : --nth 4..'}), <bang>0)]]
+vim.cmd [[command! -bang -nargs=* Rg call fzf#vim#grep('rg --column --line-number --no-heading --color=always --smart-case --hidden -- '.shellescape(<q-args>), 1, fzf#vim#with_preview(), <bang>0)]]
 
 api.nvim_set_keymap('n', '<leader>ff', ':Files!<CR>', {noremap = true})
 api.nvim_set_keymap('n', '<leader>fg', ':Rg!<CR>', {noremap = true})
