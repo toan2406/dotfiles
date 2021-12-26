3486b6150163015a0287c46c6b0433d18cd1d104
Toan Nguyen <toan.nguyen@employmenthero.com>
2021-12-26 18:09:29 +0700

    update nvim-lua config

--- nvim-lua/.config/nvim/lua/plugins/autopairs.lua
+++ nvim-lua/.config/nvim/lua/plugins/autopairs.lua
@@ -10,7 +10,8 @@ end)
 
 npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
+-- Insert `(` after select function or method item
+-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+-- local cmp = require('cmp')
+-- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
 
