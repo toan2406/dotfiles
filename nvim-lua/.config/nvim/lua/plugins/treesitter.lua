07cd86c9612c1fbabf88636fdd0bfb3c56d27dfa
Toan Nguyen <toan.nguyen@employmenthero.com>
2022-07-04 14:02:32 +0700

    update orgmode parser config

--- nvim-lua/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-lua/.config/nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,4 @@
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
-
-parser_configs.norg = {
-  install_info = {
-    url = 'https://github.com/nvim-neorg/tree-sitter-norg',
-    files = {'src/parser.c', 'src/scanner.cc'},
-    branch = 'main'
-  }
-}
-
-parser_configs.org = {
-  install_info = {
-    url = 'https://github.com/milisims/tree-sitter-org',
-    files = {'src/parser.c', 'src/scanner.cc'},
-    revision = 'main'
-  },
-  filetype = 'org'
-}
+require('orgmode').setup_ts_grammar()
 
 require('nvim-treesitter.configs').setup({
   ensure_installed = {'javascript', 'ruby', 'lua', 'bash', 'go', 'json', 'typescript', 'norg', 'org', 'cpp', 'rust'},
