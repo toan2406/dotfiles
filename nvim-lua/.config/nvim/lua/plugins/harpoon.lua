5190c68fd36548faddc5e157d0e44a6084bb242b
Toan Nguyen <toan.nguyen@employmenthero.com>
2024-08-25 02:13:11 +0700

    custom harpoon list

--- nvim-lua/.config/nvim/lua/plugins/harpoon.lua
+++ nvim-lua/.config/nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,75 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim'
   },
-  config = true,
+  opts = {
+    marks = {
+      create_list_item = function(config)
+        local Path = require('plenary.path')
+
+        local mark = vim.fn.input('Enter mark: ')
+
+        if mark == '' then
+          return nil
+        end
+
+        local current_buf = vim.api.nvim_get_current_buf()
+        local full_path = vim.api.nvim_buf_get_name(current_buf)
+        local relative_path = Path:new(full_path):make_relative(config.get_root_dir())
+
+        if relative_path == '' then
+          return nil
+        end
+
+        local current_line = vim.api.nvim_get_current_line()
+
+        local pos = vim.api.nvim_win_get_cursor(0)
+
+        local name = vim.fn.join({
+          mark,
+          '\t\t',
+          relative_path,
+          ':',
+          pos[1],
+          current_line,
+        }, '')
+
+        return {
+          value = name,
+          context = {
+            row = pos[1],
+            col = pos[2],
+            path = relative_path,
+          },
+        }
+      end,
+
+      select = function(list_item)
+        if list_item == nil then
+          return
+        end
+
+        local bufnr = vim.fn.bufnr(list_item.context.path)
+
+        if bufnr == -1 then
+          bufnr = vim.fn.bufadd(list_item.context.path)
+        end
+
+        if not vim.api.nvim_buf_is_loaded(bufnr) then
+          vim.fn.bufload(bufnr)
+          vim.api.nvim_set_option_value('buflisted', true, {
+            buf = bufnr,
+          })
+        end
+
+        vim.api.nvim_set_current_buf(bufnr)
+
+        vim.api.nvim_win_set_cursor(0, {
+          list_item.context.row,
+          list_item.context.col,
+        })
+      end
+    },
+  },
   keys = function()
     local harpoon = require('harpoon')
 
@@ -12,16 +80,23 @@ return {
       {
         '<leader>hm',
         function()
-          harpoon:list():add()
+          harpoon:list('marks'):add()
         end,
-        desc = 'Harpoon File',
+        desc = 'Harpoon Mark',
       },
       {
         '<leader>hh',
         function()
-          harpoon.ui:toggle_quick_menu(harpoon:list())
+          harpoon.ui:toggle_quick_menu(harpoon:list('marks'))
         end,
         desc = 'Harpoon Quick Menu',
+      },
+      {
+        '<leader>hc',
+        function()
+          harpoon:list('marks'):clear()
+        end,
+        desc = 'Harpoon Clear',
       },
     }
   end,
