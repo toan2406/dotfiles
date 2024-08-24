0effe624bfe61128ae6ecde66434157eee4150e9
Toan Nguyen <toan.nguyen@employmenthero.com>
2024-08-24 14:55:13 +0700

    migrate diffview to lazy plugin

--- nvim-lua/.config/nvim/lua/config/lazy.lua
+++ nvim-lua/.config/nvim/lua/config/lazy.lua
@@ -14,6 +14,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+  require('plugins.diffview'),
+
   -- Color schemes
   'jacoborus/tender.vim',
   'ellisonleao/gruvbox.nvim',
@@ -22,7 +24,7 @@ require('lazy').setup({
   'toan2406/vifm.vim',
   'justinmk/vim-sneak',
   'ggandor/leap.nvim',
-  {'junegunn/fzf', build = function() vim.fn['fzf#install']() end},
+  { 'junegunn/fzf', build = function() vim.fn['fzf#install']() end },
   'junegunn/fzf.vim',
   'kyazdani42/nvim-tree.lua',
   -- 'tpope/vim-projectionist',
@@ -33,34 +35,33 @@ require('lazy').setup({
   'hrsh7th/cmp-path',
   'hrsh7th/cmp-cmdline',
   -- Issue: https://github.com/hrsh7th/nvim-cmp/issues/1606
-  {'hrsh7th/nvim-cmp', commit = '950d0e3a93ba61c13b031c086d11eacf4bd48d24'},
+  { 'hrsh7th/nvim-cmp', commit = '950d0e3a93ba61c13b031c086d11eacf4bd48d24' },
   'hrsh7th/cmp-vsnip',
   'hrsh7th/vim-vsnip',
 
   -- Statusline
-  {'nvim-lualine/lualine.nvim', dependencies = {'kyazdani42/nvim-web-devicons', lazy = true}},
+  { 'nvim-lualine/lualine.nvim', dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true } },
 
   -- Git support
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'junegunn/gv.vim',
   'stsewd/fzf-checkout.vim',
-  {'lewis6991/gitsigns.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
-  {'sindrets/diffview.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
+  { 'lewis6991/gitsigns.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Language support
-  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', event = 'BufRead'},
-  {'nvim-treesitter/nvim-treesitter-refactor', event = 'BufRead'},
-  {'nvim-treesitter/nvim-treesitter-textobjects', event = 'BufRead'},
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate', event = 'BufRead' },
+  { 'nvim-treesitter/nvim-treesitter-refactor', event = 'BufRead' },
+  { 'nvim-treesitter/nvim-treesitter-textobjects', event = 'BufRead' },
   'neovim/nvim-lspconfig',
   'nvim-lua/lsp-status.nvim',
   'nvim-lua/lsp_extensions.nvim',
-  {'glepnir/lspsaga.nvim', branch = 'main'},
-  {'reasonml-editor/vim-reason-plus', ft = {'re', 'rei'}},
-  {'rescript-lang/vim-rescript', ft = {'res', 'resi'}},
+  { 'glepnir/lspsaga.nvim', branch = 'main' },
+  { 'reasonml-editor/vim-reason-plus', ft = { 're', 'rei' } },
+  { 'rescript-lang/vim-rescript', ft = { 'res', 'resi' } },
   'tpope/vim-rails',
-  {'elixir-editors/vim-elixir', ft = {'ex', 'exs'}},
-  {'SmiteshP/nvim-navic', dependencies = {'neovim/nvim-lspconfig'}},
+  { 'elixir-editors/vim-elixir', ft = { 'ex', 'exs' } },
+  { 'SmiteshP/nvim-navic', dependencies = { 'neovim/nvim-lspconfig' } },
   'mickael-menu/zk-nvim',
 
   -- Database
@@ -69,7 +70,7 @@ require('lazy').setup({
 
   -- Debug
   'mfussenegger/nvim-dap',
-  {'rcarriga/nvim-dap-ui', dependencies = {'mfussenegger/nvim-dap'}},
+  { 'rcarriga/nvim-dap-ui', dependencies = { 'mfussenegger/nvim-dap' } },
 
   -- Terminal
   'numtostr/FTerm.nvim',
@@ -88,11 +89,11 @@ require('lazy').setup({
         enabled = false,
         debounce = 200,
         indent = {
-          highlight = {'CursorColumn', 'Whitespace'},
+          highlight = { 'CursorColumn', 'Whitespace' },
           char = '',
         },
         whitespace = {
-          highlight = {'CursorColumn', 'Whitespace'},
+          highlight = { 'CursorColumn', 'Whitespace' },
           remove_blankline_trail = false,
         },
         scope = {
@@ -106,10 +107,10 @@ require('lazy').setup({
   'simrat39/symbols-outline.nvim',
   -- 'tversteeg/registers.nvim',
   'chentoast/marks.nvim',
-  {'folke/which-key.nvim', event = 'VeryLazy'},
-  {'ellisonleao/glow.nvim', branch = 'main'},
-  {'ThePrimeagen/harpoon', dependencies = {'nvim-lua/plenary.nvim'}},
-  {'chrisgrieser/nvim-spider', lazy = true},
+  { 'folke/which-key.nvim', event = 'VeryLazy' },
+  { 'ellisonleao/glow.nvim', branch = 'main' },
+  { 'ThePrimeagen/harpoon', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'chrisgrieser/nvim-spider', lazy = true },
   {
     'tamton-aquib/duck.nvim',
     config = function()
@@ -119,21 +120,20 @@ require('lazy').setup({
   },
 
   -- Org mode
-  {'nvim-neorg/neorg', ft = 'norg', dependencies = {'nvim-lua/plenary.nvim'}},
-  {'nvim-orgmode/orgmode', ft = 'org'},
+  { 'nvim-neorg/neorg', ft = 'norg', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-orgmode/orgmode', ft = 'org' },
 
   -- Copilot
-  {'zbirenbaum/copilot.lua', cmd = 'Copilot', event = 'InsertEnter'},
-  {'zbirenbaum/copilot-cmp', dependencies = {'zbirenbaum/copilot.lua'}},
-  {'David-Kunz/gen.nvim'},
+  { 'zbirenbaum/copilot.lua', cmd = 'Copilot', event = 'InsertEnter' },
+  { 'zbirenbaum/copilot-cmp', dependencies = { 'zbirenbaum/copilot.lua' } },
+  { 'David-Kunz/gen.nvim' },
 
   -- Local plugins
-  {dir = '~/.dotfiles/nvim-plugins/list-toggle.nvim', config = function() require('list-toggle') end},
-  {dir = '~/.dotfiles/nvim-plugins/debug-helper.nvim'},
-  {dir = '~/.dotfiles/nvim-plugins/obsidian.nvim'},
+  { dir = '~/.dotfiles/nvim-plugins/list-toggle.nvim', config = function() require('list-toggle') end },
+  { dir = '~/.dotfiles/nvim-plugins/debug-helper.nvim' },
+  { dir = '~/.dotfiles/nvim-plugins/obsidian.nvim' },
 }, {
   defaults = {
     lazy = false,
   },
 })
-
